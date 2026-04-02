cfcb2ed6cf8a6ebee60a5828214fa4c74521f5b5
morgan4h <lagosmorgan0@gmail.com>
2026-04-02 00:08:41 -0400

    add page checkLogin for client side to know if the user login or not to change the state still need to work on it but yeah good sstart

---
+++ controll/checkLogin.php
@@ -0,0 +1,83 @@
+<?php
+
+    include_once "../controll/db.php";
+?>
+
+<?php
+
+@$email = $_POST['email'];
+@$ps = $_POST['ps'];
+
+?>
+
+
+
+
+<?php
+
+
+// functions 
+
+
+function checkLogin($action) {
+    // TODO FIX THE SUCCESSFUL MEssage not show up
+   $users = array();
+   array_push($users,$action);
+//    var_dump($users);
+   if(in_array('good',$users)) {
+    header('Content-Type: application/json');
+       $good = array('state' => 'successfully', 'code' => '200', 'message' => 'you successfully login to your accont!');
+       echo json_encode($good);
+   }else {
+    header('Content-Type: application/json');
+    $message = array('state' => 'unfortunately', 'code' => '404' ,'message' => 'you unfortunately not login, try again or contac us!');
+    echo json_encode($message);
+   }
+}
+
+
+// check the information then allow login process
+
+$email = $_COOKIE['email'];
+$ps = $_COOKIE['tokenLogin'];
+
+
+
+$sql = "SELECT * FROM `users`";
+// Execute the SQL query
+$result = $conn->query($sql);
+
+// Process the result set
+if ($result->num_rows > 0) {
+  // Output data of each row
+  $callFunction;
+  while($row = $result->fetch_assoc()) {
+    // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    // echo $row['name'];
+    if(@$email == $row['email'] && @$ps == $row['pswd']) {
+      
+    //   $okayMessage = "welcome your email is  " . $_COOKIE['tokenLogin'];
+    $callFunction = 1;
+    
+      
+    //   header("Location: ../public/index.html");
+      exit();
+    }else {
+      $notOkayMessage = "not login";
+    //   echo $notOkayMessage;
+    $callFunction = 0;
+    
+    }
+  }
+  if ($callFunction == 0) {
+    checkLogin('bad');
+  }else if ($callFunction == 1) {
+    checkLogin('good');
+  }else {
+    echo "not exist option";
+  }
+} else {
+  echo "0 results";
+}
+
+?>
