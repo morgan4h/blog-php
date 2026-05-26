75e0b8b98f0e52af3e0cd7ee0348060a6b2fd550
morgan4h <lagosmorgan0@gmail.com>
2026-05-26 05:30:12 -0400

    some change about css of the dash file

--- dash/content.php
+++ dash/content.php
@@ -78,6 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>content</title>
+    <link rel="stylesheet" href="../dash/css/content.css">
 </head>
 <body>
 
--- dash/index.php
+++ dash/index.php
@@ -8,6 +8,7 @@ include_once "../controll/lock.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../dash/css/index.css">
     <title>admin</title>
 </head>
 <body>
@@ -17,6 +18,6 @@ include_once "../controll/lock.php";
     <a href="app.php">apps</a>
     <hr>
     <a href="content.php">website content</a>
-    
+    <a href="../index.html">Exist</a>
 </body>
 </html>
