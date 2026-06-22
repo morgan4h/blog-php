5c8cd9f5ee66137027e8fc6283a68b601f23c7b8
morgan4h <lagosmorgan0@gmail.com>
2026-06-22 16:06:15 -0400

    edit the json  of links by adding inst

--- js/community.js
+++ js/community.js
@@ -12,10 +12,11 @@ fetch('../../js/links.json')
     })
     .then(data => {
         console.log(data);
-        links[2].href = data.sm[0].x
-        links[3].href = data.sm[0].yt
-        links[4].href = data.sm[0].telegram
-        links[5].href = data.sm[0].email
+        links[2].href = data.sm[0].inst
+        links[3].href = data.sm[0].x
+        links[4].href = data.sm[0].yt
+        links[5].href = data.sm[0].telegram
+        links[6].href = data.sm[0].email
         // Your logic for the links data goes here
     }) // Fixed the closing sequence here
     .catch(error => {
